dd69495ca553533a25a87e1c7996a6338820ba10
cfern052 <cfern052@gmail.com>
2019-06-27 14:57:21 -0400

    updated interface and added js

---
+++ script.ts
@@ -0,0 +1,33 @@
+//interface
+
+interface DesiredDataShape {
+    firstName: string | 'N/A';
+    lastName: string | 'N/A';
+    phone: number | 'N/A';
+    email: string | 'N/A';
+}
+
+
+var fullData:string[];
+
+let data=[
+    { lastName: "Alberston", phone: "555-255-5555", email: "name.a@email.com" },
+    { firstName: "Ringo", lastName: "Starter", phone: "555-455-5555", email: "ringo@email.com" },
+    { firstName: "Courtney McKenzie", lastName: "", phone: "555-545-5555", email: "ringo@email" },
+    { firstName: null, lastName: "Dave Flippin", phone: "555-226-5555", email: null },
+    { firstName: "Paul", lastName: "Allan", phone: "555-255-5555", email: "paul@email.com"},
+    { phone: "555-555-5555", email: "george@email.com" },
+    { firstName: "Jerry", phone: "555-355-5555", email: "jerry@email.com" },
+    { firstName: "Sterling", lastName: "Loggins", email: "johnsemail.com" },
+    { firstName: "George", lastName: "Harrold", phone: "555-555-5555", email: "george@email.com" },
+    { firstName: "Mary", lastName: "Fallen", phone: "5551555555", email: "mary@email.com" },
+    { firstName: "John", lastName: "Stalin", phone: "555-355-5555", email: "john@email.com" },
+    { firstName: "Jim", lastName: "Onfire" }
+]
+
+function displayData(options:DesiredDataShape) {
+let firstName=options.firstName
+let lastName=options.lastName
+let phone=options.phone
+
+}
